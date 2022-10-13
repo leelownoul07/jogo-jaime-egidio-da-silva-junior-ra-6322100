15801e41808a06d7053a84de682420a5eedb235c
jaime <105826772+leelownoul07@users.noreply.github.com>
2022-10-12 21:20:21 -0300

    first commit

---
+++ var.js
@@ -0,0 +1,31 @@
+var Jogador1;
+var Jogador2;
+
+
+function Escolha1(){
+
+    Jogador1 = "1";
+}
+
+function Escolha2(){
+
+    Jogador1 = "2";
+}
+function Escolha3(){
+
+    Jogador1 = "3";
+}
+
+function Escolha1(){
+
+    Jogador2 = "4";
+}
+function Escolha2(){
+
+    Jogador2 = "5"; 
+}
+
+function Escolha3(){
+
+    Jogador2 = "6";
+}
